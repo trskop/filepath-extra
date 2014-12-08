5e67d5418746db209c34f1f7cd9cfe83a283c567
Peter Trško <peter.trsko@gmail.com>
2014-12-08 21:25:26 +0100

    Haddock comments

--- src/System/FilePath/Extra.hs
+++ src/System/FilePath/Extra.hs
@@ -35,6 +35,14 @@ import System.IO (FilePath)
 import System.FilePath ((</>), joinPath, splitDirectories)
 
 
+-- | Drop prefix of a file path.
+--
+-- >>> dropPrefix "/home/joe" "/home/joe/devel/haskell/mega-project"
+-- Just "devel/haskell/mega-project"
+-- >>> dropPrefix "joe" "joe/devel/haskell/mega-project"
+-- Just "devel/haskell/mega-project"
+-- >>> dropPrefix "/home/joe" "devel/haskell/mega-project"
+-- Nothing
 dropPrefix :: FilePath -> FilePath -> Maybe FilePath
 dropPrefix prefix path = dropPrefix' prefix' path'
   where
@@ -47,14 +55,32 @@ dropPrefix prefix path = dropPrefix' prefix' path'
       | x == y    = dropPrefix' xs ys
       | otherwise = Nothing
 
+-- | Unsafe version of 'dropPrefix'. It throws error when path is not starting
+-- with prefix.
 unsafeDropPrefix :: FilePath -> FilePath -> FilePath
 unsafeDropPrefix prefix path = case dropPrefix prefix path of
     Just fp -> fp
     Nothing -> error "unsafeDropPrefix: Path doesn't start with prefix."
 
-changePrefixTo :: FilePath -> FilePath -> FilePath -> Maybe FilePath
-changePrefixTo oldPrefix newPrefix = fmap (newPrefix </>) . dropPrefix oldPrefix
+-- | First drop \"old\" prefix and then insert new one.
+--
+-- @
+-- changePrefixTo oldPrefix newPrefix =
+--     'fmap' (newPrefix '</>') '.' 'dropPrefix' oldPrefix
+-- @
+changePrefixTo
+    :: FilePath
+    -- ^ Old prefix.
+    -> FilePath
+    -- ^ New prefix.
+    -> FilePath
+    -- ^ Path starting with old prefix.
+    -> Maybe FilePath
+changePrefixTo oldPrefix newPrefix =
+    fmap (newPrefix </>) . dropPrefix oldPrefix
 
+-- | Unsafe version of 'changePrefixTo'. It throws error when path is not
+-- starting with prefix.
 unsafeChangePrefixTo :: FilePath -> FilePath -> FilePath -> FilePath
 unsafeChangePrefixTo oldPrefix newPrefix path =
     case changePrefixTo oldPrefix newPrefix path of
@@ -62,11 +88,51 @@ unsafeChangePrefixTo oldPrefix newPrefix path =
         Nothing -> error
             "unsafeChangePrefixTo: Path doesn't start with prefix."
 
+-- | Check if path starts with prefix.
+--
+-- @
+-- path `hasPrefix` prefix <==> 'isJust' ('dropPrefix' prefix path)
+-- @
 hasPrefix :: FilePath -> FilePath -> Bool
 hasPrefix = (isJust .) . dropPrefix
 
+-- | Drop specified number of directories from the start of a path.
+--
+-- Note that if path is absolute (starting with "/" on UNIX/Linux), then it is
+-- considered as a first directory.
+--
+-- >>> dropDirectories 1 "/home/joe/devel/haskell/mega-project"
+-- "home/joe/devel/haskell/mega-project"
+-- >>> dropDirectories 2 "/home/joe/devel/haskell/mega-project"
+-- "joe/devel/haskell/mega-project"
+-- >>> dropDirectories 3 "/home/joe/devel/haskell/mega-project"
+-- "devel/haskell/mega-project"
+-- >>> dropDirectories 100 "/home/joe/devel/haskell/mega-project"
+-- ""
+-- >>> dropDirectories 0 "/home/joe/devel/haskell/mega-project"
+-- "/home/joe/devel/haskell/mega-project"
+-- >>> dropDirectories (-10) "/home/joe/devel/haskell/mega-project"
+-- "/home/joe/devel/haskell/mega-project"
+-- >>> dropDirectories 2 "devel/haskell/mega-project"
+-- "mega-project"
 dropDirectories :: Int -> FilePath -> FilePath
 dropDirectories n = joinPath . drop n . splitDirectories
 
+-- | Take specified number of directories from the start of a path.
+--
+-- >>> takeDirectories 1 "/home/joe/devel"
+-- "/"
+-- >>> takeDirectories 2 "/home/joe/devel"
+-- "/home"
+-- >>> takeDirectories 3 "/home/joe/devel"
+-- "/home/joe"
+-- >>> takeDirectories 10 "/home/joe/devel"
+-- "/home/joe/devel"
+-- >>> takeDirectories 0 "/home/joe/devel"
+-- ""
+-- >>> takeDirectories (-10) "/home/joe/devel"
+-- ""
+-- >>> takeDirectories 2 "usr/local/bin"
+-- "usr/local"
 takeDirectories :: Int -> FilePath -> FilePath
 takeDirectories n = joinPath . take n . splitDirectories
